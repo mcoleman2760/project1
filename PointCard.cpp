222f5dc5793bb091867b586225491fc3649429a4
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-11-07 00:23:05 -0500

    Update PointCard.cpp

--- PointCard.cpp
+++ PointCard.cpp
@@ -3,22 +3,27 @@
 
 
 PointCard::PointCard(){
-    std::cout << "Constructor for PointCard" << std::endl;
+    setType(POINT_CARD);
 }
 
 bool PointCard::isPlayable() {
-    if ((getDrawn() == true) && (std::stoi(getInstruction()) >= 1 && std::stoi(getInstruction()) <= 99 )){
-        return true;
+    if (getDrawn() == true){
+        return false;
+    } 
+    if(!(std::stoi(getInstruction()) >= 1 && std::stoi(getInstruction()) <= 99)){
+        return false;
     }
-    return false;
+    return true;
 } 
 
 void PointCard::Print() const {
-    std::cout << "Type: [PointCard]\nInstruction:[";
-    std::cout << getInstruction() + "]\nCard:\n[";
+    std::cout << "Type: [" << getType() << "]" << std::endl;   
+    std::cout << "Points: [" << getInstruction() << "]" << std::endl; 
+    std::cout << "Card: " << std::endl;
+    std::cout << "[";
     if (getImageData() == 0){
         std::cout << "No image data" << std::endl;
     }
-    std::cout << getImageData() << std::endl;
+    std::cout << getImageData()  <<  "]" << std::endl;
 
 }
