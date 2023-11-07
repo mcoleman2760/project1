9b1905bc4f962978078a13af9c1a451f4c5deb05
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-11-07 00:21:28 -0500

    Update ActionCard.cpp

--- ActionCard.cpp
+++ ActionCard.cpp
@@ -1,24 +1,30 @@
 #include "ActionCard.hpp"
 
   ActionCard::ActionCard(){
+    setType(ACTION_CARD);
    
   }
 
    bool ActionCard::isPlayable() {
-     if ((getDrawn() == true) && (getInstruction() == "REVERSE HAND" || "SWAP HAND WITH OPPONENT" || "DRAW x CARD(S)" || "PLAY x CARD(S)")){
-     return true;
+     if (getDrawn() == false){
+        return false;
      }
+     if (getInstruction() != "REVERSE HAND" || getInstruction() != "SWAP HAND WITH OPPONENT" || getInstruction().substr(0,4) != "DRAW" || getInstruction().substr(0,4) != "PLAY"){
      return false;
+     }
+     return true;
   }
 
 
    void ActionCard::Print() const {
-    std::cout << "Type: [ActionCard]\nInstruction:[";
-    std::cout << getInstruction() + "]\nCard:\n[";
+    std::cout << "Type: [" << getType() << "]" << std::endl;
+    std::cout << "Instruction: [" << getInstruction() << "]" << std::endl;
+    std::cout << "Card: " << std::endl;
+    std::cout << "[";
     if (getImageData() == 0){
-        std::cout << "No image data" << std::endl;
+        std::cout << "No image data]" << std::endl;
     }
-    std::cout << getImageData() << std::endl;
+    std::cout << getImageData() <<  "]" <<std::endl;
 
 
   }
