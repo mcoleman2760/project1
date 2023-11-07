1d75621200717ba90f4c7576979b557d7d72f4ea
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-11-07 01:20:23 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "Hand.hpp"
+#include "Player.hpp"
+#include "ActionCard.hpp"
+#include "PointCard.hpp"
+#include "Card.hpp"
+#include "Deck.hpp"
+
+int main(){
+  Player p1;
+  
+    
+
+    
+    p1.setScore(5);
+    std::cout << p1.getScore() << std::endl;
+   
+    
+
+
+
+    return 0;
+}
