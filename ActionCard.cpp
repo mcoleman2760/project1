9a6108fe22615937cecfd5280b00ee8a8b5be06c
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-11-08 00:24:36 -0500

    Update ActionCard.cpp

--- ActionCard.cpp
+++ ActionCard.cpp
@@ -20,8 +20,8 @@
 
 
    void ActionCard::Print() const {
-    std::cout << "Type: [" << getType() << "]" << std::endl;
-    std::cout << "Instruction: [" << getInstruction() << "]" << std::endl;
+    std::cout << "Type: " << getType() << std::endl;
+    std::cout << "Instruction: " << getInstruction()  << std::endl;
     std::cout << "Card: " << std::endl;
     
     const int* data = getImageData();
