0579fea299a9c191649ed7ac16928c9902981977
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-11-09 13:06:50 -0500

    Update PointCard.hpp

--- PointCard.hpp
+++ PointCard.hpp
@@ -1,35 +1,38 @@
-#include "PointCard.hpp"
+/*
+CSCI335 Fall 2023
+Assignment 1 – Card Game
+Name
+Date
+PointCard.hpp defines the PointCard class and inherits from the Card Class.
+*/
+#ifndef POINTCARD_HPP
+#define POINTCARD_HPP
 
+#include <iostream>
+#include <string>
+#include "Card.hpp"
 
-PointCard::PointCard(){
-    setType(POINT_CARD);
-}
-//Checks if the Card is Playable
-bool PointCard::isPlayable() {
-    if (getDrawn() == false ){ //|| getInstruction().size() > 2){ // Checks if the PointCard is Drawn 
-        return false;
-    } 
- 
-    if(std::stoi(getInstruction()) < 1 || std::stoi(getInstruction()) > 99){ // Checks if the PointCard is between values 1 and 99
-        return false;
-    }
-
-    return true;
-} 
-// Prints the information on the PointCard
-void PointCard::Print() const {
-    std::cout << "Type: " << getType()  << std::endl;   
-    std::cout << "Points: " << getInstruction() << std::endl; 
-    std::cout << "Card: " << std::endl;
-    const int* imgd = getImageData();
-    if (imgd != nullptr){
-        for (int i = 0; i < sizeof(imgd); i++){
-            std::cout << imgd[i];
-        }
-    }
-    else{
-    std::cout << "No image data" << std::endl;
-    }
-    
-
-}
+class PointCard : public Card
+{
+    public:
+        /**
+         * @post: Construct a new Point Card object
+         */
+        PointCard();
+        /**
+         * @return true if the card is playable, false otherwise
+         * For a card to be playable, it has to be drawn and the instruction has to be a valid number
+        */
+        bool isPlayable() override; 
+        /**
+         * @post: Print the Point Card in the following format:
+         * Type: [CardType]
+         * Points: [Instruction]
+         * Card: 
+         * [ImageData]
+         * 
+         * Note: For [ImageData]: If there is no image data, print "No image data" instead
+         */
+        void Print() const override;
+};
+#endif
