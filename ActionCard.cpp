00e5626404a47efc94c791ec178b69c1c15f149e
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-11-09 13:15:59 -0500

    Update ActionCard.cpp

--- ActionCard.cpp
+++ ActionCard.cpp
@@ -1,39 +1,38 @@
 #include "ActionCard.hpp"
 
   ActionCard::ActionCard(){
-    setType(ACTION_CARD);
+    setType(ACTION_CARD);  //Constructor
    
   }
 
    bool ActionCard::isPlayable() {
-     if (getDrawn() == false || getInstruction().size() < 11 ){
+      if (getDrawn() == false || getInstruction().size() < 11 ){ // Checks if the ActionCard is drawn
         return false;
-     }
-     if (getInstruction() == "REVERSE HAND" || getInstruction() == "SWAP HAND WITH OPPONENT" || getInstruction().substr(0,4) == "DRAW" || getInstruction().substr(0,4) == "PLAY"){
-     return true;
+      }
+     if (getInstruction() == "REVERSE HAND" || getInstruction() == "SWAP HAND WITH OPPONENT" || getInstruction().substr(0,4) == "DRAW" || getInstruction().substr(0,4) == "PLAY"){ // Checks for the first word of the instruction.
+        return true;
      }
      else{
-      return false;
+        return false;
      }
      
   }
 
-
-   void ActionCard::Print() const {
-    std::cout << "Type: " << getType() << std::endl;
-    std::cout << "Instruction: " << getInstruction()  << std::endl;
-    std::cout << "Card: " << std::endl;
-    
-    const int* data = getImageData();
-    if (getImageData() == nullptr){
+// Prints out the information for the ActionCard
+    void ActionCard::Print() const {
+      std::cout << "Type: " << getType() << std::endl; // Prints Card Type
+      std::cout << "Instruction: " << getInstruction()  << std::endl; // Prints Instruction
+      std::cout << "Card: " << std::endl;
+    // Gets image data
+      const int* data = getImageData();
+      if (getImageData() == nullptr){ // if theres no image data then print out No image date
         std::cout << "No image data" << std::endl;
-    }
-    else{
-
-      for(int i = 0; i < sizeof(getImageData()); i++){
-        std::cout << data[i] << std::endl;
+      }
+      else{
+        for(int i = 0; i < sizeof(getImageData()); i++){  // prints the numbers in the Image Data
+          std::cout << data[i] << std::endl;
       }
       
     }
-    
+   
   }
